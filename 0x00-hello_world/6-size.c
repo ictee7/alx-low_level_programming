ec67346a4c352613546744eb55115923515761c9
Ian Chirwa <chirwaian7@gmail.com>
2022-10-13 09:28:06 -0700

    mod of 6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,12 +12,12 @@ int main(void)
 	int i;
 	long int li;
 	long long int lli;
-	float f;
+	float fl;
 
-	printf("Size of a char : %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of an int : %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of a long int : %lu byte(s)\n", (unsigned long)sizeof(li));
-	printf("Size of a long long int : %lu byte(s)\n", (unsigned long)sizeof(lli));
-	printf("Size of a float : %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char : %c byte(s)\n", sizeof(c));
+	printf("Size of an int : %d byte(s)\n", sizeof(i));
+	printf("Size of a long int : %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int : %zu byte(s)\n", sizeof(lli));
+	printf("Size of a float : %zu byte(s)\n", sizeof(fl));
 	return (0);
 }
