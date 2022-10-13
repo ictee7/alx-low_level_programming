1e9e286697e94b1506a28cd4f83a613209fe2bb7
Ian Chirwa <chirwaian7@gmail.com>
2022-10-13 10:11:03 -0700

    mod of 6 file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	long long int lli;
 	float fl;
 
-	printf("Size of a char : %d byte(s)\n", sizeof(c));
-	printf("Size of an int : %d byte(s)\n", sizeof(i));
-	printf("Size of a long int : %d byte(s)\n", sizeof(li));
-	printf("Size of a long long int : %d byte(s)\n", sizeof(lli));
-	printf("Size of a float : %d byte(s)\n", sizeof(fl));
+	printf("Size of a char : %ld byte(s)\n", sizeof(c));
+	printf("Size of an int : %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int : %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int : %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float : %ld byte(s)\n", sizeof(fl));
 	return (0);
 }
