25db1bcbfa527aa04d8285a3e9506cbed4018449
Ian Chirwa <chirwaian7@gmail.com>
2022-10-14 04:07:37 -0700

    added 4-print_alphabt.c file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+*main - Entry Point
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch < 'z')
+	{
+		if (ch == 'e')
+		{
+			ch++;
+		}
+		else if (ch == 'q')
+		{
+			ch++;
+		}
+		else
+		{
+			putchar(ch);
+			ch++;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
