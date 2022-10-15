0515cfd42a5d65710fb5e9b4a490b7e7f6c7c759
Ian Chirwa <chirwaian7@gmail.com>
2022-10-15 11:14:18 -0700

    mod of 101-quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
 *main - Entry Point
 *
@@ -8,6 +8,7 @@
 
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
