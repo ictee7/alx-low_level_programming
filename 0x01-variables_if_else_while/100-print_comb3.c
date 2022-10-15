c3a020a702817a3040adbd6e14515aaf2e9e7dc7
Ian Chirwa <chirwaian7@gmail.com>
2022-10-15 03:08:44 -0700

    modified 100-print_comb3.c file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
 /**
-*main - Entry Point
-*
-*Return: Always 0 (Success)
-*/
+  *main - Entry Point
+  *
+  *Return: Alaways 0 (Success)
+  */
 
 int main(void)
 {
@@ -13,20 +12,46 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = '1'; j <= '9'; j++)
+		j = '0';
+
+		if (i == '0')
+			j = '1';
+		else if (i == '1')
+			j = '2';
+		else if (i == '2')
+			j = '3';
+		else if (i == '3')
+			j = '4';
+		else if (i == '4')
+			j = '5';
+		else if (i == '5')
+			j = '6';
+		else if (i == '6')
+			j = '7';
+		else if (i == '7')
+			j = '8';
+		else if (i == '8')
+			j = '9';
+		else
+			break;
+
+		for (; j <= '9'; j++)
 		{
 			putchar(i);
 			putchar(j);
-			if ( ij == 11)
+
+			if (i == '8')
 			{
 				break;
 			}
+
 			else
 			{
-			putchar(',');
-			putchar(' ');
+				putchar(',');
+				putchar(' ');
 			}
 		}
+
 	}
 	putchar('\n');
 	return (0);
