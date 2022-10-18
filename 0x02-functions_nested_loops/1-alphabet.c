8747055085685859837d9476c0dc0a6c98d41265
Ian Chirwa <chirwaian7@gmail.com>
2022-10-18 18:34:13 +0200

    added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+/**
+ *main - Entry Point
+ *
+ *Return: Always 0
+ */
+#include "main.h"
+
+void print_alphabet(void)
+{
+	char i;
+	
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
