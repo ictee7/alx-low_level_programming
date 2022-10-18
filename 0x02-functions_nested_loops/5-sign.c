0471762c2c67bb6b4dcdcc5da7a1014c5118ed59
Ian Chirwa <chirwaian7@gmail.com>
2022-10-18 22:21:15 +0200

    added 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+/**
+ *print_sign - Entry Point
+ *@n: Parameter to pass
+ *Return: 0 or 1
+ */
+#include "main.h"
+#include <stdlib.h>
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+}
+
