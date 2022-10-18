738123859152e3d0641edd3cb61b7cfb2926053b
Ian Chirwa <chirwaian7@gmail.com>
2022-10-18 23:49:10 +0200

    added 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+/**
+ *jack_bauer - Entry Point
+ *
+ *Return: 0
+ */
+#include "main.h"
+
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			for (k = 0; k < 6; k++)
+				for (l = 0; l < 10; l++)
+				{
+					_putchar('0' + i);
+					_putchar('0' + j);
+					_putchar(':');
+					_putchar('0' + k);
+					_putchar('0' + l);
+					_putchar('\n');
+				}
+
+		}
+	}
+}
