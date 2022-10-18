a46a50069619bd1b6afa154706995d546f5466b6
Ian Chirwa <chirwaian7@gmail.com>
2022-10-18 18:37:41 +0200

    modified 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,5 @@
 /**
- *main - Entry Point
+ *print_alphabet - Entry Point
  *
  *Return: Always 0
  */
@@ -8,7 +8,7 @@
 void print_alphabet(void)
 {
 	char i;
-	
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
