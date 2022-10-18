684e24181f64b6f402a132f61a1ccb9f6e9ab9e2
Ian Chirwa <chirwaian7@gmail.com>
2022-10-19 00:54:06 +0200

    added 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *times_table - Entry Point
+ *
+ *Return: 0
+ */
+
+
+void times_table(void)
+{
+	_putchar('n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,4 +9,5 @@ int print_sign(int n);
 int _abs(int);
 void jack_bauer(void);
 int add(int, int);
+void times_table(void);
 #endif
