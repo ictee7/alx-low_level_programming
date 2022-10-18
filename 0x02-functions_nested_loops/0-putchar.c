aef4f57ac62cc4b8c69bcabbc59498dce23d5de6
Ian <chirwaian7@gmail.com>
2022-10-18 16:24:14 +0200

    modified 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,19 @@
+/**
+  *main - Entry Point
+  *
+  *Return: Always 0.
+  */
+
 #include "main.h"
 
 int main(void)
 {
 	int i;
-	char c[9] = "_putchar";
+	char c[] = "_putchar";
 
 	for (i = 0; i < 9; i++)
 	{
 		_putchar (c[i]);
 	}
-	return(0);
+	return (0);
 }
