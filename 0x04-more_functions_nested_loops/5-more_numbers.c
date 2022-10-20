37809c8195c67652627693063bc394b54f8cc8eb
Ian Chirwa <chirwaian7@gmail.com>
2022-10-20 20:16:58 +0200

    modified 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,23 +13,11 @@ void more_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 20; j++)
+		for (j = 0; j < 15; j++)
 		{
-			if (j == 10 || j == 12 || j == 13 || j == 14 || j == 16 || j == 18)
-				j = 1;
-			else if (j == 11)
-				j = 0;
-			else if (j == 15)
-				j = 2;
-			else if (j == 17)
-				j = 3;
-			else if (j == 19)
-				j = 4;
-			else if (j == 20)
-				break;
-			else
-				_putchar(j + '0');
-			_putchar(j + '0');
+			if (j > 9)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 
