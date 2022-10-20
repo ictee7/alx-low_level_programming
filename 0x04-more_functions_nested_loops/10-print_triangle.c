2e7eff4581025359bc479a0333dbb04c999167b1
Ian Chirwa <chirwaian7@gmail.com>
2022-10-20 21:44:12 +0200

    added 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  *print_triangle - Function that prints a triangle
+  *@size: parameter to pass
+  *Return: void
+  */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+		_putchar('#');
+	_putchar('\n');
+}
+
