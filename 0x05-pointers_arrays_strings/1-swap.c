c1d7e8e1f01acf7818fbc8940ec4a82ae40605ed
Ian Chirwa <chirwaian7@gmail.com>
2022-10-24 11:30:02 +0200

    added 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *swap_int - Function that swaps the values of two integers
+ *@a: 1st parameter passed
+ *@b: 2nd parameter passed
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
