84c58ceea023fa703cb4f7536eb23c3583a2147b
Ian Chirwa <chirwaian7@gmail.com>
2022-10-24 12:49:03 +0200

    added 3-puts.c file

---
+++ .ccls-cache/@home@ian@Documents@ALx@Projects@alx-low_level_programming/0x05-pointers_arrays_strings@1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *swap_int - Function that swaps the values of two integers
+ *@a: 1st parameter passed
+ *@b: 2nd parameter passed
+ */
+
+void swap_int(int *a, int *b)
+{
+
+	a = *b;
+	b = *a;
+}
---
+++ .ccls-cache/@home@ian@Documents@ALx@Projects@alx-low_level_programming/0x05-pointers_arrays_strings@3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_puts - Function that prints a string
+ *@str: Parameter passed
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+}
