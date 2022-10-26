5ea5ff62f98cca1d8abfdb33411cab26f0839021
Ian Chirwa <chirwaian7@gmail.com>
2022-10-26 15:13:36 +0200

    added 3-strcmp.c file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_strcmp - Function that compares two strings
+ *@s1: 1st parameter
+ *@s2: 2nd parameter
+ *Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while ((s1[i] != '\0') || (s2[i] != '\0'))
+	{
+		if (s1[i] > s2[i])
+			return (15);
+		if (s1[i] < s2[i])
+			return (-15);
+		i++;
+	}
+	return (0);
+}
