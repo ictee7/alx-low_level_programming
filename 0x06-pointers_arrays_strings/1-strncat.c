2f2a4d136e9496caab3e70274438779c7da8f86f
Ian Chirwa <chirwaian7@gmail.com>
2022-10-27 12:26:02 +0200

    modified 1-strncat.c file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,8 +11,6 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
-	int len = 0;
-	int len2 = 0;
 	int dest_len = 0;
 
 	while (dest[i] != '\0')
@@ -21,13 +19,5 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[dest_len + i] = src[i];
-	len = i;
-	len2 = i - 1;
-
-	if (len < n)
-		dest[dest_len + len] = src[len];
-	else
-		dest[dest_len + len2] = src[len2];
-		
 	return (dest);
 }
