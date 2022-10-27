5c172862c0ae95f37137a26edc04b790ad4dcf8b
Ian Chirwa <chirwaian7@gmail.com>
2022-10-27 12:42:12 +0200

    modified 1-strncat.c file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,20 +11,14 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	int src_len = 0;
 
 	while (i != '\0')
 		i++;
-	src_len = i;
 
 	for (i = 0; i < n && src[i]; i++)
 		dest[i] = src[i];
 
-	if (n > src_len)
-	{
+	for ( ; i < n; i++)
 		dest[i] = src[i];
-		return (dest);
-	}
-	else
-		return (dest);
+	return (dest);
 }
