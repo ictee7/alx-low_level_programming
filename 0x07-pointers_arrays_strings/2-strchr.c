77f8d6e20e9ca845f7f28c62604b96e868c95db9
Ian Chirwa <chirwaian7@gmail.com>
2022-10-31 13:05:40 +0200

    modified 2-strchr.c file

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  *_strchr - Function that locates a char in a string
@@ -14,5 +15,5 @@ char *_strchr(char *s, char c)
 		if (*s == c)
 			return (s);
 	}
-	return (0);
+	return (NULL);
 }
