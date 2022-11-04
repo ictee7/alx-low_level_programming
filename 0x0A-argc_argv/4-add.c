564511e4386857ddd526807a5845e0e3b9255224
Ian Chirwa <chirwaian7@gmail.com>
2022-11-04 19:39:43 +0200

    added 4-add.c file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Program that add positive numbers
+ *@argc: First parameter passed
+ *@argv: 2nd parameter passed
+ *Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+	int sum;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum = sum + atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c~
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Program that multiplies two numbers
+ *@argc: First parameter passed
+ *@argv: 2nd parameter passed
+ *Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int res;
+	int first;
+	int last;
+
+	if (argc != 3)
+		printf("Error\n");
+	else
+	{
+		first = atoi(argv[1]);
+		last = atoi(argv[2]);
+		res = first * last;
+		printf("%d\n", res);
+	}
+	return (0);
+}
