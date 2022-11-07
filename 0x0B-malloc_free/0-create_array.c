d9aeb2fc4cff38cfdcdaed16ee397a1922c16af5
Ian Chirwa <chirwaian7@gmail.com>
2022-11-07 12:18:59 +0200

    mod of 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 
 	str = malloc(sizeof(char) * size);
 
-	if (size == 0)
+	if (size == 0 || str == 0)
 	{
 		printf("failed to allocate memory");
 		return (0);
