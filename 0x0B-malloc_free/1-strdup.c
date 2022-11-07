90f37d329159959b05d69a6f4bf05748bedd7717
Ian Chirwa <chirwaian7@gmail.com>
2022-11-07 13:16:56 +0200

    added 1-strdup.c file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strdup - Function that returns a pointer to a newly allocated space
+ *@str: string passed
+ *Return: pointer to duplicated string or null
+ */
+
+char *_strdup(char *str)
+{
+	char *p_str = malloc(strlen(str + 1));
+
+	if (p_str == 0)
+	{
+		return (0);
+	}
+	else
+		strcpy(p_str, str);
+
+	return (p_str);
+	free(p_str);
+}
