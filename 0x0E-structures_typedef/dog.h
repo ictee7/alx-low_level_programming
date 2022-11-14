4e79914d008d14efe727cc2b7a8fbb322107e9b5
Ian Chirwa <chirwaian7@gmail.com>
2022-11-14 15:25:22 +0200

    added dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  *struct dog - Entry point
+  *@age: 1st member
+  *@name: 2nd member
+  *@owner: 3rd member
+  */
+
+struct dog
+/* structure*/
+{
+	float age;
+	char *name;
+	char *owner;
+	/* structure containing members*/
+};
+
+#endif
