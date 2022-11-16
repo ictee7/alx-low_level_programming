838842ed447319920dcc0988884bd560d694f8f6
Ian Chirwa <chirwaian7@gmail.com>
2022-11-16 16:50:44 +0200

    added 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,13 +13,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
-		if (array[i] == 98)
-			break;
-		if (array[i] == -98)
-			return(i);
-		cmp(array[i]);
+		if (cmp(array[i]) != 0)
+			return (i);
 	}
-	return (i);
+	return (-1);
 }
